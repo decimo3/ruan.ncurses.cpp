3a4a26c8c09e0067d733a4ba45426b4abef6a820
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2022-01-22 19:48:16 -0300

    DONE: Amateur attempt to center text in window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <ncurses.h>
+#include <iostream>
+#include <string>
 
 int main () {
+  int maxRow, maxCol, halfRow, halfCol, posicao;
+  std::string texto;
   initscr();
-  mvprintw(80,10, "Hello World !!!");
+  maxRow = LINES - 1;
+  maxCol = COLS - 1;
+  halfCol = maxCol / 2;
+  halfRow = maxRow / 2;
+  texto = "Colunas: " + std::to_string(maxCol) + " Linhas: " + std::to_string(maxRow);
+  posicao = halfCol - (texto.length() / 2);
+  mvprintw(halfRow, posicao, "Colunas: %d Linhas: %d", maxCol, maxRow);
   refresh();
   getch();
   endwin();
